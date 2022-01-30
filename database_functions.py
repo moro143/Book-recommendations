9925c7a8f9f89ec67e9dce8b8b5110c80a93ebb4
Mateusz Moroszczuk <moro143moro@gmail.com>
2022-01-31 00:22:39 +0100

    Books likness functions

--- database_functions.py
+++ database_functions.py
@@ -43,7 +43,28 @@ def get_all_books_column(column_name):
     conn.commit()
     return result   
 
-def get_most_popular_categories(n=10, column_categories="subjects"):
+def get_all_books_columns(column_names=[]):
+    string_column_names = ""
+    for i in column_names:
+        string_column_names += i +", "
+    command = f"SELECT {string_column_names[:-2]} FROM books;"
+    
+    conn = psycopg2.connect(
+            host=SERVER_HOST,
+            database=DATABASE,
+            user=USER,
+            password=PASSWORD
+        )
+    cur = conn.cursor()
+    cur.execute(command)
+    
+    result = cur.fetchall()
+    
+    cur.close()
+    conn.commit()
+    return result  
+
+def get_most_popular_categories(n=-1, column_categories="subjects"):
     books = get_all_books_column(column_categories)
     categories = []
     for book in books:
@@ -54,8 +75,34 @@ def get_most_popular_categories(n=10, column_categories="subjects"):
     sorted_books = [x for _, x in sorted(zip(counts, category))]
     sorted_books.reverse()
     return sorted_books[:n]
-    
-    
 
-print(get_most_popular_categories(50))
-        
+def score_likeness_by_category(categories=get_most_popular_categories(20)):
+    books = get_all_books_columns(["book_id", "subjects"])
+    categories = set(categories)
+    scores = {}
+    n=0
+    for book1 in books:
+        print(n/len(books))
+        n+=1
+        if book1[1] != None:
+            for book2 in books[n:]:
+                if book2[1] != None:
+                    cat_book1 = set(book1[1])
+                    cat_book2 = set(book2[1])
+                    score = len(cat_book1.intersection(categories.intersection(cat_book2)))
+                    scores[(book1[0], book2[0])] = score
+                    #print(score)
+                    # if book1[1] != None and book2[1] != None:
+                    #     for cat in book1[1]:
+                    #         if cat in categories and cat in book2[1]:
+                    #             score+=1
+                    #     sco   res[(book1[0], book2[0])] = score
+    n=0
+    for i in scores.keys():
+        if scores[i]>n:
+            n=scores[i]
+            print(i, scores[i])
+    return scores
+            
+
+scores = score_likeness_by_category()
